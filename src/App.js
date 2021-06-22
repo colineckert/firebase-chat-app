4fa10cbf2e37e5dba0c3183f4d394e9be82a6743
colineckert <colineckert10@gmail.com>
2021-06-22 08:39:32 -0700

    Init and setup firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDgpGeCZuS8z4JoAxaKFEP-GAt67K2eYK4",
+  authDomain: "superchat-13784.firebaseapp.com",
+  projectId: "superchat-13784",
+  storageBucket: "superchat-13784.appspot.com",
+  messagingSenderId: "448953794783",
+  appId: "1:448953794783:web:913687c384d8b1f658a96a"
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+
 function App() {
+
+  const [user] = useAuthState();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
+      
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
     </div>
   );
+}
+
+function SignIn() {
+  
 }
 
 export default App;
